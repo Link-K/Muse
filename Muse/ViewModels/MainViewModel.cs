f9c0cb8872bd37b1b8c7e30f243aa71d8fe1ef59
若渊 <1127654811@qq.com>
2026-05-29 22:13:11 +0800

    feat: 实现编辑/分屏/阅读模式切换及分屏方向切换功能

--- Muse/ViewModels/MainViewModel.cs
+++ Muse/ViewModels/MainViewModel.cs
@@ -1,9 +1,83 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace Muse.ViewModels;
 
 public partial class MainViewModel : ViewModelBase
 {
-    [ObservableProperty]
-    private string _greeting = "Welcome to Avalonia!";
+	[ObservableProperty]
+	private EditorMode _currentMode = EditorMode.Edit;
+
+	[ObservableProperty]
+	private SplitOrientation _currentSplitOrientation = SplitOrientation.Horizontal;
+
+	public string HeaderText => CurrentMode switch
+	{
+		EditorMode.Edit => "编辑模式（默认）",
+		EditorMode.Split => "分屏模式",
+		_ => "阅读模式"
+	};
+
+	public bool IsEditMode => CurrentMode == EditorMode.Edit;
+
+	public bool IsSplitMode => CurrentMode == EditorMode.Split;
+
+	public bool IsReadMode => CurrentMode == EditorMode.Read;
+
+	public bool IsSplitHorizontal => CurrentSplitOrientation == SplitOrientation.Horizontal;
+
+	public bool IsSplitVertical => CurrentSplitOrientation == SplitOrientation.Vertical;
+
+	[RelayCommand]
+	private void SwitchToEditMode()
+	{
+		CurrentMode = EditorMode.Edit;
+	}
+
+	[RelayCommand]
+	private void SwitchToSplitMode()
+	{
+		CurrentMode = EditorMode.Split;
+	}
+
+	[RelayCommand]
+	private void SwitchToReadMode()
+	{
+		CurrentMode = EditorMode.Read;
+	}
+
+	[RelayCommand]
+	private void ToggleSplitOrientation()
+	{
+		CurrentSplitOrientation = CurrentSplitOrientation == SplitOrientation.Horizontal
+			? SplitOrientation.Vertical
+			: SplitOrientation.Horizontal;
+	}
+
+	partial void OnCurrentModeChanged(EditorMode value)
+	{
+		OnPropertyChanged(nameof(HeaderText));
+		OnPropertyChanged(nameof(IsEditMode));
+		OnPropertyChanged(nameof(IsSplitMode));
+		OnPropertyChanged(nameof(IsReadMode));
+	}
+
+	partial void OnCurrentSplitOrientationChanged(SplitOrientation value)
+	{
+		OnPropertyChanged(nameof(IsSplitHorizontal));
+		OnPropertyChanged(nameof(IsSplitVertical));
+	}
+}
+
+public enum EditorMode
+{
+	Edit,
+	Split,
+	Read
+}
+
+public enum SplitOrientation
+{
+	Horizontal,
+	Vertical
 }
