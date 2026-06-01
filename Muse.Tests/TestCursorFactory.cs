6943bc858b9042a3d474a3c7b09a5a4636cc9064
若渊 <1127654811@qq.com>
2026-06-01 20:04:16 +0800

    docs: 更新实施计划，新增 S2-004 变更说明并标记其为进行中

--- Muse.Tests/TestCursorFactory.cs
+++ Muse.Tests/TestCursorFactory.cs
@@ -3,21 +3,21 @@ using Avalonia.Platform;
 
 namespace Muse.Tests
 {
-    public class TestCursorFactory : ICursorFactory
-    {
-        public ICursorImpl GetCursor(StandardCursorType cursorType)
-        {
-            return new TestCursorImpl();
-        }
+	public class TestCursorFactory : ICursorFactory
+	{
+		public ICursorImpl GetCursor(StandardCursorType cursorType)
+		{
+			return new TestCursorImpl();
+		}
 
-        public ICursorImpl CreateCursor(IBitmapImpl cursor, PixelPoint hotSpot)
-        {
-            return new TestCursorImpl();
-        }
+		public ICursorImpl CreateCursor(IBitmapImpl cursor, PixelPoint hotSpot)
+		{
+			return new TestCursorImpl();
+		}
 
-        private class TestCursorImpl : ICursorImpl
-        {
-            public void Dispose() { }
-        }
-    }
+		private class TestCursorImpl : ICursorImpl
+		{
+			public void Dispose() { }
+		}
+	}
 }
