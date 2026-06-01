5f66fb5a92e36dd2e3ca73639a84de4939f04779
若渊 <1127654811@qq.com>
2026-06-01 20:40:10 +0800

    feat: 统一代码风格，调整 SafeGridSplitter 和 MainView.axaml 中的缩进和格式

--- Muse.Tests/PreviewBlockViewModelTests.cs
+++ Muse.Tests/PreviewBlockViewModelTests.cs
@@ -7,107 +7,107 @@ using Xunit;
 
 namespace Muse.Tests
 {
-    public sealed class PreviewBlockViewModelTests : IDisposable
-    {
-        private readonly IServiceProvider? _originalServiceProvider;
+	public sealed class PreviewBlockViewModelTests : IDisposable
+	{
+		private readonly IServiceProvider? _originalServiceProvider;
 
-        public PreviewBlockViewModelTests()
-        {
-            _originalServiceProvider = GetServiceProvider();
-        }
+		public PreviewBlockViewModelTests()
+		{
+			_originalServiceProvider = GetServiceProvider();
+		}
 
-        public void Dispose()
-        {
-            SetServiceProvider(_originalServiceProvider);
-        }
+		public void Dispose()
+		{
+			SetServiceProvider(_originalServiceProvider);
+		}
 
-        [Fact]
-        public async Task CopyCodeCommand_SetsClipboardText()
-        {
-            var tcs = new TaskCompletionSource<string?>();
-            var fake = new RecordingClipboardService(tcs);
+		[Fact]
+		public async Task CopyCodeCommand_SetsClipboardText()
+		{
+			var tcs = new TaskCompletionSource<string?>();
+			var fake = new RecordingClipboardService(tcs);
 
-            var services = new ServiceCollection();
-            services.AddSingleton<IClipboardService>(fake);
-            SetServiceProvider(services.BuildServiceProvider());
+			var services = new ServiceCollection();
+			services.AddSingleton<IClipboardService>(fake);
+			SetServiceProvider(services.BuildServiceProvider());
 
-            var block = new RenderedBlock(RenderedBlockKind.CodeFence, "```", "console.log('hi');", 1);
-            var vm = new Muse.ViewModels.PreviewBlockViewModel(block);
+			var block = new RenderedBlock(RenderedBlockKind.CodeFence, "```", "console.log('hi');", 1);
+			var vm = new Muse.ViewModels.PreviewBlockViewModel(block);
 
-            vm.CopyCodeCommand.Execute(null);
+			vm.CopyCodeCommand.Execute(null);
 
-            var result = await Task.WhenAny(tcs.Task, Task.Delay(2000));
-            Assert.True(tcs.Task.IsCompleted, "Clipboard was not invoked within timeout");
-            Assert.Equal("console.log('hi');", tcs.Task.Result);
-        }
+			var result = await Task.WhenAny(tcs.Task, Task.Delay(2000));
+			Assert.True(tcs.Task.IsCompleted, "Clipboard was not invoked within timeout");
+			Assert.Equal("console.log('hi');", tcs.Task.Result);
+		}
 
-        [Fact]
-        public async Task CopyAnchorCommand_WhenHeading_SetsSlug()
-        {
-            var tcs = new TaskCompletionSource<string?>();
-            var fake = new RecordingClipboardService(tcs);
+		[Fact]
+		public async Task CopyAnchorCommand_WhenHeading_SetsSlug()
+		{
+			var tcs = new TaskCompletionSource<string?>();
+			var fake = new RecordingClipboardService(tcs);
 
-            var services = new ServiceCollection();
-            services.AddSingleton<IClipboardService>(fake);
-            SetServiceProvider(services.BuildServiceProvider());
+			var services = new ServiceCollection();
+			services.AddSingleton<IClipboardService>(fake);
+			SetServiceProvider(services.BuildServiceProvider());
 
-            var block = new RenderedBlock(RenderedBlockKind.Heading, "# Title", "Title Example", 1);
-            var vm = new Muse.ViewModels.PreviewBlockViewModel(block);
+			var block = new RenderedBlock(RenderedBlockKind.Heading, "# Title", "Title Example", 1);
+			var vm = new Muse.ViewModels.PreviewBlockViewModel(block);
 
-            vm.CopyAnchorCommand.Execute(null);
+			vm.CopyAnchorCommand.Execute(null);
 
-            var result = await Task.WhenAny(tcs.Task, Task.Delay(2000));
-            Assert.True(tcs.Task.IsCompleted, "Clipboard was not invoked for heading");
-            Assert.Equal("title-example", tcs.Task.Result);
-        }
+			var result = await Task.WhenAny(tcs.Task, Task.Delay(2000));
+			Assert.True(tcs.Task.IsCompleted, "Clipboard was not invoked for heading");
+			Assert.Equal("title-example", tcs.Task.Result);
+		}
 
-        [Fact]
-        public async Task CopyAnchorCommand_WhenNotHeading_DoesNotCallClipboard()
-        {
-            var tcs = new TaskCompletionSource<string?>();
-            var fake = new RecordingClipboardService(tcs);
+		[Fact]
+		public async Task CopyAnchorCommand_WhenNotHeading_DoesNotCallClipboard()
+		{
+			var tcs = new TaskCompletionSource<string?>();
+			var fake = new RecordingClipboardService(tcs);
 
-            var services = new ServiceCollection();
-            services.AddSingleton<IClipboardService>(fake);
-            SetServiceProvider(services.BuildServiceProvider());
+			var services = new ServiceCollection();
+			services.AddSingleton<IClipboardService>(fake);
+			SetServiceProvider(services.BuildServiceProvider());
 
-            var block = new RenderedBlock(RenderedBlockKind.Paragraph, "para", "Just text", 1);
-            var vm = new Muse.ViewModels.PreviewBlockViewModel(block);
+			var block = new RenderedBlock(RenderedBlockKind.Paragraph, "para", "Just text", 1);
+			var vm = new Muse.ViewModels.PreviewBlockViewModel(block);
 
-            vm.CopyAnchorCommand.Execute(null);
+			vm.CopyAnchorCommand.Execute(null);
 
-            // wait a short time; the clipboard should not be called
-            await Task.Delay(250);
-            Assert.False(tcs.Task.IsCompleted, "Clipboard should not be invoked for non-heading");
-        }
+			// wait a short time; the clipboard should not be called
+			await Task.Delay(250);
+			Assert.False(tcs.Task.IsCompleted, "Clipboard should not be invoked for non-heading");
+		}
 
-        private static IServiceProvider? GetServiceProvider()
-        {
-            var prop = typeof(Muse.App).GetProperty("ServiceProvider", System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Static);
-            return prop?.GetValue(null) as IServiceProvider;
-        }
+		private static IServiceProvider? GetServiceProvider()
+		{
+			var prop = typeof(Muse.App).GetProperty("ServiceProvider", System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Static);
+			return prop?.GetValue(null) as IServiceProvider;
+		}
 
-        private static void SetServiceProvider(IServiceProvider? provider)
-        {
-            var prop = typeof(Muse.App).GetProperty("ServiceProvider", System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Static);
-            var setter = prop?.GetSetMethod(nonPublic: true);
-            setter?.Invoke(null, new object?[] { provider });
-        }
+		private static void SetServiceProvider(IServiceProvider? provider)
+		{
+			var prop = typeof(Muse.App).GetProperty("ServiceProvider", System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Static);
+			var setter = prop?.GetSetMethod(nonPublic: true);
+			setter?.Invoke(null, new object?[] { provider });
+		}
 
-        private sealed class RecordingClipboardService : IClipboardService
-        {
-            private readonly TaskCompletionSource<string?> _tcs;
+		private sealed class RecordingClipboardService : IClipboardService
+		{
+			private readonly TaskCompletionSource<string?> _tcs;
 
-            public RecordingClipboardService(TaskCompletionSource<string?> tcs)
-            {
-                _tcs = tcs;
-            }
+			public RecordingClipboardService(TaskCompletionSource<string?> tcs)
+			{
+				_tcs = tcs;
+			}
 
-            public Task<bool> SetTextAsync(string text)
-            {
-                _tcs.TrySetResult(text);
-                return Task.FromResult(true);
-            }
-        }
-    }
+			public Task<bool> SetTextAsync(string text)
+			{
+				_tcs.TrySetResult(text);
+				return Task.FromResult(true);
+			}
+		}
+	}
 }
--- Muse/Controls/SafeGridSplitter.cs
+++ Muse/Controls/SafeGridSplitter.cs
@@ -6,54 +6,54 @@ using Avalonia.VisualTree;
 
 namespace Muse.Controls
 {
-    public class SafeGridSplitter : ContentControl
-    {
-        public static readonly StyledProperty<GridResizeDirection> ResizeDirectionProperty =
-            AvaloniaProperty.Register<SafeGridSplitter, GridResizeDirection>(nameof(ResizeDirection), GridResizeDirection.Columns);
+	public class SafeGridSplitter : ContentControl
+	{
+		public static readonly StyledProperty<GridResizeDirection> ResizeDirectionProperty =
+			AvaloniaProperty.Register<SafeGridSplitter, GridResizeDirection>(nameof(ResizeDirection), GridResizeDirection.Columns);
 
-        public static readonly StyledProperty<GridResizeBehavior> ResizeBehaviorProperty =
-            AvaloniaProperty.Register<SafeGridSplitter, GridResizeBehavior>(nameof(ResizeBehavior), GridResizeBehavior.PreviousAndNext);
+		public static readonly StyledProperty<GridResizeBehavior> ResizeBehaviorProperty =
+			AvaloniaProperty.Register<SafeGridSplitter, GridResizeBehavior>(nameof(ResizeBehavior), GridResizeBehavior.PreviousAndNext);
 
-        public GridResizeDirection ResizeDirection
-        {
-            get => GetValue(ResizeDirectionProperty);
-            set => SetValue(ResizeDirectionProperty, value);
-        }
+		public GridResizeDirection ResizeDirection
+		{
+			get => GetValue(ResizeDirectionProperty);
+			set => SetValue(ResizeDirectionProperty, value);
+		}
 
-        public GridResizeBehavior ResizeBehavior
-        {
-            get => GetValue(ResizeBehaviorProperty);
-            set => SetValue(ResizeBehaviorProperty, value);
-        }
+		public GridResizeBehavior ResizeBehavior
+		{
+			get => GetValue(ResizeBehaviorProperty);
+			set => SetValue(ResizeBehaviorProperty, value);
+		}
 
-        public SafeGridSplitter()
-        {
-            AttachedToVisualTree += OnAttachedToVisualTree;
-        }
+		public SafeGridSplitter()
+		{
+			AttachedToVisualTree += OnAttachedToVisualTree;
+		}
 
-        private void OnAttachedToVisualTree(object? sender, VisualTreeAttachmentEventArgs e)
-        {
-            // Only attempt to create the real GridSplitter at runtime when attached to visual tree
-            try
-            {
-                var gs = new Avalonia.Controls.GridSplitter();
+		private void OnAttachedToVisualTree(object? sender, VisualTreeAttachmentEventArgs e)
+		{
+			// Only attempt to create the real GridSplitter at runtime when attached to visual tree
+			try
+			{
+				var gs = new Avalonia.Controls.GridSplitter();
 
-                // copy basic layout properties
-                gs.Width = this.Width;
-                gs.Height = this.Height;
-                gs.Background = this.Background;
+				// copy basic layout properties
+				gs.Width = this.Width;
+				gs.Height = this.Height;
+				gs.Background = this.Background;
 
-                // apply our styled properties
-                gs.ResizeDirection = this.ResizeDirection;
-                gs.ResizeBehavior = this.ResizeBehavior;
+				// apply our styled properties
+				gs.ResizeDirection = this.ResizeDirection;
+				gs.ResizeBehavior = this.ResizeBehavior;
 
-                // set as content so it participates in layout
-                this.Content = gs;
-            }
-            catch
-            {
-                // If platform services are not available (headless tests), leave Content null
-            }
-        }
-    }
+				// set as content so it participates in layout
+				this.Content = gs;
+			}
+			catch
+			{
+				// If platform services are not available (headless tests), leave Content null
+			}
+		}
+	}
 }
